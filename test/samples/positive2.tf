75617f56d9302533cd9aea020515f2b596f36876
neilcar <me@neilcar.com>
2023-04-06 17:39:42 -0400

    Update positive2.tf

--- test/samples/positive2.tf
+++ test/samples/positive2.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "positive1" {
 }
 
 resource "azurerm_container_registry" "positive2" {
-  name                     = "containerRegistry1"
+  name                     = "containerRegistry12"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   sku                      = "Premium"
